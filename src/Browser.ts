8edf71f5a203baeae78eb88638c48f29ea4966cc
chunza2542 <chunza2542@hotmail.com>
2019-06-26 18:05:06 +0700

    Login to Facebook and Send Message

--- src/Browser.ts
+++ src/Browser.ts
@@ -1,5 +1,15 @@
+import puppeteer from "puppeteer";
+
 import IConfig from "./domains/IConfig";
 import Utils from "./utils/Utils";
+
+const sleep = async (ms: number) => {
+    return new Promise((res, _) => {
+        setTimeout(() => {
+            res();
+        }, ms);
+    });
+};
 
 class Browser {
     private message: string;
@@ -12,7 +22,48 @@ class Browser {
 
     public async run() {
         const ids: string[] = this.urls.map(Utils.extractID);
-        console.log(ids);
+
+        const browser = await puppeteer.launch({headless: false});
+        const page = await browser.newPage();
+
+        await this.login(page);
+        await this.sendMessage(ids, 0, page);
+
+        await browser.close();
+        console.log("[Log] Done~");
+    }
+
+    private async login(page) {
+        await page.goto(`https://www.facebook.com/`, {
+            waitUntil: "networkidle2",
+        });
+
+        await page.waitForSelector("#email");
+        await page.type("#email", process.env.EMAIL);
+        await page.type("#pass", process.env.PASSWORD);
+
+        await sleep(500);
+
+        await page.click("#loginbutton");
+    }
+
+    private async sendMessage(ids: string[], index: number, page) {
+        if (index >= ids.length) {
+            return;
+        }
+
+        await page.goto(`https://www.facebook.com/messages/t/${ids[index]}`, {
+            waitUntil: "networkidle2",
+        });
+
+        await page.keyboard.type(this.message);
+        await sleep(100);
+        page.keyboard.press(String.fromCharCode(13));;
+
+        console.log(`[Log] Send message to ${ids[index]}`);
+
+        await sleep(500);
+        await this.sendMessage(ids, index + 1, page);
     }
 }
 
